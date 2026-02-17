5fb03912c7c460237298ce3f514a558fe2079dc4
ZEUS <zouhairsahtout007@gmail.com>
2026-02-17 12:06:29 +0800

    Complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
 
+    let output = '';
+    for (let i = 0; i < num; i++) {
+        output += str;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
