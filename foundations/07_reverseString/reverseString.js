8c66eb4c59100398cb5a753eb40230b085bb7284
ZEUS <zouhairsahtout007@gmail.com>
2026-02-17 12:21:50 +0800

    Complete 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str
+        .split('')
+        .reverse()
+        .join('');
 };
 
 // Do not edit below this line
