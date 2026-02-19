266b3035762ec084dcdbc30758183092f79c6e59
ZEUS <zouhairsahtout007@gmail.com>
2026-02-19 09:01:19 +0800

    Complete 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const result = (fahrenheit - 32) / 1.8;
+  const isZero = Math.floor((result * 10) % 10);
+
+  if (!isZero) return result;
+  else return +result.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const result = celsius * 1.8 + 32;
+  const isZero = Math.floor((result * 10) % 10);
+
+  if (!isZero) return result;
+  else return +result.toFixed(1);
 };
 
 // Do not edit below this line
